c0c8058e497631160cd7b150642565e26d179163
mmariaa12 <marievich.ru@mail.ru>
2022-12-13 04:02:20 +0545

    Чистка

--- Task2/Helpers/Helpers.cs
+++ Task2/Helpers/Helpers.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.IO;
 using System.Linq;
 using System.Security.Cryptography;
-using System.Windows.Media.Imaging;
 
 namespace ArcFaceWPF.Helpers
 {
@@ -18,35 +16,28 @@ namespace ArcFaceWPF.Helpers
             );
         }
 
-        public static BitmapImage ByteToBitmap(byte[] array)
-        {
-            using var ms = new MemoryStream(array);
-            var image = new BitmapImage();
-            image.BeginInit();
-            image.CacheOption = BitmapCacheOption.OnLoad;
-            image.StreamSource = ms;
-            image.EndInit();
-            return image;
-        }
-
         public static float[]? ByteToFloat(byte[]? array)
         {
             if (array == null)
+            {
                 return null;
-            var len = array.Length;
-            var float_array = new float[len / 4];
-            Buffer.BlockCopy(array, 0, float_array, 0, len);
+            }
+               
+            var float_array = new float[array.Length / 4];
+            Buffer.BlockCopy(array, 0, float_array, 0, array.Length);
             return float_array;
         }
 
         public static byte[]? FloatToByte(float[]? array)
         {
             if (array == null)
+            {
                 return null;
-
-            var byte_array = new byte[array.Length * 4];
-            Buffer.BlockCopy(array, 0, byte_array, 0, byte_array.Length);
-            return byte_array;
+            }
+                
+            var result = new byte[array.Length * 4];
+            Buffer.BlockCopy(array, 0, result, 0, result.Length);
+            return result;
         }
     }
 }
